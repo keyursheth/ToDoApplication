19802009f746a6ad278f90530a5c88b4467795f9
keyur <keyursheth3@gmail.com>
2020-05-25 16:07:27 +0530

    added sorting

--- Frontend/React/to-do-app/src/Components/Tasks/TasksListNew.js
+++ Frontend/React/to-do-app/src/Components/Tasks/TasksListNew.js
@@ -8,46 +8,69 @@ const TasksListNew = (props) => {
         props.onTaskUpdate(item);
     }
 
+    const displayTask = (item) => {
+
+        const taskCompleteStyle = (item.isComplete === false ? '' : 'line-through');
+        const editTaskPath = `/taskedit/${item.taskId}`;
+        
+        return(
+            <li key={item.taskId}>    
+                <input 
+                    type="checkbox" 
+                    checked={item.isComplete} 
+                    onChange={() => taskCompleteHandler(item) }
+                /> 
+                <span
+                    style={{textDecoration:taskCompleteStyle}}>
+                        {item.taskDescription}
+                </span>
+                {(item.isComplete === false) ?
+                    <Link to={editTaskPath}>
+                        <input 
+                            type="button" 
+                            value="edit" 
+                            style={{marginLeft:'10px'}} 
+                        /> 
+                    </Link>
+                    : null
+                }
+                <input 
+                    type="button" 
+                    value="delete" 
+                    style={{marginLeft:'10px'}} 
+                    onClick={() => { props.onTaskDelete(item.taskId) }}
+                />                                
+            </li>
+        );
+    }
+
     return (
         <div>
             <h1>Tasks List</h1>
             <ul style={{listStyleType:'none', paddingLeft:'0px'}}>
                 {
-                    props.todoItems.map((item) => {
+                    props.todoItems
+                    .sort((a, b) => a.taskDescription.localeCompare(b.taskDescription))
+                    .map((item) => {
 
-                        const taskCompleteStyle = (item.isComplete === false ? '' : 'line-through');
-                        const editTaskPath = `/taskedit/${item.taskId}`;
-                        
-                        return (
-                            <li key={item.taskId}>    
-                                <input 
-                                    type="checkbox" 
-                                    checked={item.isComplete} 
-                                    onChange={() => taskCompleteHandler(item) }
-                                /> 
-                                <span
-                                    style={{textDecoration:taskCompleteStyle}}>
-                                        {item.taskDescription}
-                                </span>
-                                {(item.isComplete === false) ?
-                                    <Link to={editTaskPath}>
-                                        <input 
-                                            type="button" 
-                                            value="edit" 
-                                            style={{marginLeft:'10px'}} 
-                                        /> 
-                                    </Link>
-                                    : null
-                                }
-                                <input 
-                                    type="button" 
-                                    value="delete" 
-                                    style={{marginLeft:'10px'}} 
-                                    onClick={() => { props.onTaskDelete(item.taskId) }}
-                                />                                
-                            </li>
-                        )}
-                    )
+                        if (item.isComplete === true) {
+                            return null;
+                        }
+
+                        return displayTask(item)
+                    })
+                }
+                {
+                    props.todoItems
+                    .sort((a, b) => a.taskDescription.localeCompare(b.taskDescription))
+                    .map((item) => {
+
+                        if (item.isComplete !== true) {
+                            return null;
+                        }
+
+                        return displayTask(item)
+                    })
                 }
             </ul>
         </div>
